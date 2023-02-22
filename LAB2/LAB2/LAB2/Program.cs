738d58c9134c4f1c52978c5ef512ecb13e83bbc8
Artyom-Gerchik <bombruchelyator@gmail.com>
2023-02-22 23:00:44 +0300

    initiall commit

---
+++ LAB2/LAB2/LAB2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace LAB2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var varTypesDict = new Dictionary<string, string>()
+        {
+            { "int", "variable type" },
+            { "float", "variable type" },
+            { "char", "variable type" },
+            { "string", "variable type" },
+        };
+        var keywordsDict = new Dictionary<string, string>()
+        {
+            { "const", "key word" },
+            { "if", "key word" },
+            { "else", "key word" },
+            { "else if", "key word" },
+            { "do", "key word" },
+            { "while", "key word" },
+            { "for", "key word" },
+            { "return", "key word" },
+            { "#include", "key word" },
+            { "#pragma", "key word" },
+            { "<iostream>", "key word" },
+            { "<math.h>", "key word" },
+            { "using", "key word" },
+            { "namespace", "key word" },
+            { "break", "key word" },
+            { "continue", "key word" },
+        };
+        var specSymbolsDict = new Dictionary<string, string>()
+        {
+            { "<<", "spec symbol" },
+            { ">>", "spec symbol" },
+            { "{", "spec symbol" },
+            { "}", "spec symbol" },
+            { "[", "spec symbol" },
+            { "]", "spec symbol" },
+            { ";", "spec symbol" },
+        };
+    }
+}
