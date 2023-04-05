a856b641fc540731cce15d07e42f966f550158a6
Artyom-Gerchik <bombruchelyator@gmail.com>
2023-04-06 01:21:00 +0300

    lab5 working on executor

--- LAB5/Executor/Functional/Executor.cs
+++ LAB5/Executor/Functional/Executor.cs
@@ -8,6 +8,7 @@ public class Executor
     private AbstractNode Root { get; set; }
     private Dictionary<string, Dictionary<string, object>> VariableTables { get; set; } = new();
     private Semantic Semantic { get; set; }
+    private string CodeBlock { get; set; }
 
     public Executor(AbstractNode root, Dictionary<string, Dictionary<string, string>> variableTables, Semantic semantic)
     {
@@ -24,13 +25,15 @@ public class Executor
         }
 
         Semantic = semantic;
+        CodeBlock = "NONE";
     }
 
     public void Execute()
     {
+        WorkOnNode(Root);
     }
 
-    public object? ExecuteNode(AbstractNode? abstractNode)
+    public object? WorkOnNode(AbstractNode? abstractNode)
     {
         if (abstractNode == null)
         {
@@ -39,9 +42,47 @@ public class Executor
 
         if (abstractNode is StatementsNode statementsNode)
         {
-            
+        }
+
+        if (abstractNode is VariableTypeNode)
+        {
+            return null;
         }
 
         return null;
+    }
+
+    public string GetCodeBlock()
+    {
+        return CodeBlock;
+    }
+
+    public void ExecuteNode(AbstractNode? abstractNode)
+    {
+        if (abstractNode == null)
+        {
+            return;
+        }
+
+        if (abstractNode is StatementsNode)
+        {
+            //TODO
+        }
+
+        if (abstractNode is ForNode forNode)
+        {
+            ExecuteNode(forNode.Body);
+        }
+
+        if (abstractNode is IfNode ifNode)
+        {
+            ExecuteNode(ifNode.Body);
+            ExecuteNode(ifNode.ElseBody);
+        }
+
+        if (abstractNode is WhileNode whileNode)
+        {
+            ExecuteNode(whileNode.Body);
+        }
     }
 }
